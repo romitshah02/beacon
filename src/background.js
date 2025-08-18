6074686aaa396a7b0ea61a02dc08b77143368a73
romit shah <romitshah02@gmail.com>
2025-08-18 22:09:06 +0530

    simplification part

--- src/background.js
+++ src/background.js
@@ -2,6 +2,12 @@ import browser from "webextension-polyfill";
 
 console.log("Hello from the background!");
 
+chrome.action.onClicked.addListener((tab) => {
+  // Send a message to the active tab to toggle the overlay
+  chrome.tabs.sendMessage(tab.id, { type: "TOGGLE_OVERLAY" });
+});
+
+
 browser.runtime.onInstalled.addListener((details) => {
   console.log("Extension installed:", details);
 });
